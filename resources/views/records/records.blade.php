0a6a49726533b7a4d90e41348a41094f4483101d
Yurii Alekseev <y.alekseev@2-berega.ru>
2017-04-10 12:40:34 +0300

    gulp + отображение врачей

--- resources/views/records/records.blade.php
+++ resources/views/records/records.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
-
+	<div class="container">
+		<div class="col-sm-6">
+			<table class="table table-hover table-pointer">
+				<thead>
+					<tr>
+						<th colspan="2">
+							Выберите врача
+						</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+					@foreach ($professions as $profession)
+						@foreach ($profession->doctors as $doctor)
+							<tr>
+								<td>{{ $profession->name }}</td>
+								<td>{{ $doctor->name }}</td>
+							</tr>
+						@endforeach
+					@endforeach
+				</tbody>
+				
+			</table>
+		</div>
+	</div>
 
 @endsection
